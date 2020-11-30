e93c3d972d7ab1e887f93cb5ce24823c80fc9414
Angel Junior Abreu <20187321@itla.edu.do>
2020-11-30 17:37:26 -0400

    crud  de servicios

---
+++ ProyectoFinalProgIII/Controllers/ServiciosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinalProgIII.Data;
+
+namespace ProyectoFinalProgIII.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiciosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Servicios.ToListAsync());
+        }
+
+        // GET: Servicios/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicios = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.ServiciosId == id);
+            if (servicios == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicios);
+        }
+
+        // GET: Servicios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiciosId,NombreS,Descripcion,Valor,CantVendidos")] Servicios servicios)
+        {
+            if (ModelState.IsValid)
+            {
+                servicios.ServiciosId = Guid.NewGuid();
+                _context.Add(servicios);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicios);
+        }
+
+        // GET: Servicios/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicios = await _context.Servicios.FindAsync(id);
+            if (servicios == null)
+            {
+                return NotFound();
+            }
+            return View(servicios);
+        }
+
+        // POST: Servicios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ServiciosId,NombreS,Descripcion,Valor,CantVendidos")] Servicios servicios)
+        {
+            if (id != servicios.ServiciosId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servicios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiciosExists(servicios.ServiciosId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicios);
+        }
+
+        // GET: Servicios/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicios = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.ServiciosId == id);
+            if (servicios == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicios);
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var servicios = await _context.Servicios.FindAsync(id);
+            _context.Servicios.Remove(servicios);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiciosExists(Guid id)
+        {
+            return _context.Servicios.Any(e => e.ServiciosId == id);
+        }
+    }
+}
